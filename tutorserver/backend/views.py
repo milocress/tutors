c44015561ef1190eb3f39f954cf38e02d74b75ae
Milo Cress <cress@mit.edu>
2021-04-12 02:34:59 -0700

    added login option

--- tutorserver/backend/views.py
+++ tutorserver/backend/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 
 from backend.models import Student, Tutor, Subject, SessionRequest
+from django.contrib.auth.decorators import login_required
 
 def root_view(request):
     print(Student.objects.all())
     return HttpResponse("Index page")
 
+@login_required
 def student_view(request):
     response = HttpResponse(content_type="text/plain")
     default_student = Student.objects.get(name="Milo Cress")
@@ -19,6 +21,7 @@ def student_view(request):
     response.write('<button>Request</button> \n')
     return response
 
+@login_required
 def tutor_view(request):
     response = HttpResponse(content_type="text/plain")
     default_tutor = Tutor.objects.get(name="Teddy Schoenfeld")
--- tutorserver/tutorserver/urls.py
+++ tutorserver/tutorserver/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('backend.urls'))
+    path('', include('backend.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     #path('admin/', admin.site.urls),
 ]
