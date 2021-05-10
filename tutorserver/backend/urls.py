cd06f956c48b23945c38935ff6adc6946c41855d
Milo Cress <cress@mit.edu>
2021-05-09 17:09:57 -0700

    added rest api bindings

--- tutorserver/backend/urls.py
+++ tutorserver/backend/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from backend import views
+from . import views
 
 urlpatterns = [
     path('', views.root_view),
     path('student/', views.student_view),
     path('tutor/', views.tutor_view),
     path('request/<int:rid>/', views.request_view),
-    path('accept-request/<int:rid>', views.accept_request_view)
+    path('accept-request/<int:rid>', views.accept_request_view),
+    path('api/students', views.StudentListCreate.as_view()),
+    path('api/sessions', views.SessionListCreate.as_view()),
+    path('api/requests', views.RequestListCreate.as_view()),
+    path('api/tutors',   views.TutorListCreate.as_view()),
 ]
