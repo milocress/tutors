61cba1e05c12ea3f79504309f8fb59745f90a94b
Milo Cress <cress@mit.edu>
2021-05-14 14:13:24 -0700

    api token auth

--- tutorserver/backend/urls.py
+++ tutorserver/backend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     path('api/tutors',   views.TutorListCreate.as_view()),
     path('api/users', views.UserListCreate.as_view()),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', auth_views.obtain_auth_token),
 ]
